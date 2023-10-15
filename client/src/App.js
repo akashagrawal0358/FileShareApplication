480ae786fcdda7f5c09ddcbd1b4221d2557bcb6e
Akash Agrawal <akashagrawal0358@gmail.com>
2023-10-15 14:41:08 +0530

    completed work of sending file from front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,65 @@
 import './App.css';
-import { useRef } from 'react' ;
+import { useRef, useState, useEffect } from 'react';
+import uploadFile from './services/api';
 
 function App() {
 
   const logoUrl = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg';
-  
-  const fileInput = useRef() ;
-  
-  const onUploadClick = ()=>{
-     fileInput.current.click();
+  const fileInput = useRef();
+
+
+  const onUploadClick = () => {
+    fileInput.current.click();
   }
 
-  return ( 
+
+  const [file, setFile] = useState("");
+  console.log(file);
+
+
+  useEffect(() => {
+    const getData = async () => {
+
+      if (file) {
+        const data = new FormData();
+        data.append("name", file.name);
+        data.append("file", file);
+      
+      //  'uploadFile func'  calls and post data 
+      let response = await uploadFile(data) ; 
+      }
+    }
+    getData() ;
+
+
+    // runs every time when file changes
+  }, [file])
+
+
+  return (
 
     // In container one half for logImg and other for file
     <div className='container'>
-    <img src={logoUrl} className='img' alt="Img Not Found" />
-    <div className='wrapper'>
-      <h1>Simple file sharing!</h1>
-      <p>Upload and share the download link.</p>
-      
-      <button onClick={ ()=> onUploadClick()} >Upload</button>
-      {/* When we click on upload button then it refers input element */}
-      <input
-        type="file"
-        ref={fileInput}
-        // display:none -->  to not show input element
-        style={{ display: "none" }}
-      />
+      <img src={logoUrl} className='img' alt="Img Not Found" />
+      <div className='wrapper'>
+        <h1>Simple file sharing!</h1>
+        <p>Upload and share the download link.</p>
 
+        <button onClick={() => onUploadClick()} >Upload</button>
+        {/* When we click on upload button then it refers input element */}
+        <input
+          type="file"
+          ref={fileInput}
+          // display:none -->  to not show input element
+          style={{ display: "none" }}
+          // e.target.value not works here 
+          // Use e.target.files where files is an array
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+
+      </div>
     </div>
-  </div>
   );
 }
 
 export default App;
- 
---
+++ client/src/services/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_URL = "http://localhost:8000" ;
+
+const uploadFile = async (data) => {
+    try {
+        //  axios return response object
+        //  response contains data key 
+        let response = await axios.post(`${API_URL}/upload`, data);
+        return response.data ;
+    } catch (error) {
+        console.error("Error Occurred ", error.message);
+    }
+
+
+};
+
+export default uploadFile;
