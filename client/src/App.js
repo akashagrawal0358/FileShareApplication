33295dc142f0123b2dc3954450e3c148c03cfd25
Akash Agrawal <akashagrawal0358@gmail.com>
2023-10-15 09:53:34 +0530

    Completed all layout and CSS parts

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     App
+
+  const logoUrl = 'https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg';
+
+  return ( 
+
+    // In container one half for logImg and other for file
+    <div className='container'>
+    <img src={logoUrl} className='img' alt="Img Not Found" />
+    <div className='wrapper'>
+      <h1>Simple file sharing!</h1>
+      <p>Upload and share the download link.</p>
+      
+      <button >Upload</button>
+      <input
+        type="file"
+        style={{ display: "none" }}
+      />
+
     </div>
+  </div>
   );
 }
 
