c315b0b0d9a4eebec2fbdc5a9e7aef97f4eea0e8
Akash Agrawal <akashagrawal0358@gmail.com>
2023-10-16 16:46:33 +0530

    UploadedFile can be downloaded from database

--- server/controller/file_controller.js
+++ server/controller/file_controller.js
@@ -1,34 +1,52 @@
-
 const File = require('../models/file.js')
 
-const uploadData = async (request , response)=>{
-  
-   //  request contains all data 
-  //   console.log(request);
 
 
- // fileObj stores what we need  
- const fileObj = {
-    path : request.file.path,
-    name : request.file.originalname
-   }
-   // console.log(fileObj) ;
+const uploadData = async (request, response) => {
 
-   try{
-    // validate fileObj and saves in DB
-    const file = await File.create(fileObj);
-    console.log(file);
+    //  request contains all data 
+    //   console.log(request);
 
-    // file, name of collection
-    response.status(200).json({path: `http://localhost:8000/file/${file._id}`})
-   }
-   catch(error){
-    console.error("Error Occurred" , error.message )
-    response.status(500).json({error: error.message })
-   }
-  
+
+    // fileObj stores what we need  
+    const fileObj = {
+        path: request.file.path,
+        name: request.file.originalname
+    }
+    // console.log(fileObj) ;
+
+    try {
+        // validate fileObj and saves in DB
+        const file = await File.create(fileObj);
+        console.log(file);
+
+        // file, name of collection
+        response.status(200).json({ path: `http://localhost:8000/file/${file._id}` })
+    }
+    catch (error) {
+        console.error("Error Occurred", error.message)
+        response.status(500).json({ error: error.message })
+    }
+
 }
 
+const downloadData = async (request, response) => {
+    try {
+        const file = await File.findById(request.params.fileId);
+        file.downloadContent++;
 
+        await file.save();
 
-module.exports = uploadData ;
+        // To download an file
+        response.download(file.path, file.name);
+    }
+    catch (error) {
+        console.error(error.message);
+        return response.status(500).json({ error: error.message });
+    }
+}
+
+//module.exports = downloadData ;
+//module.exports =  uploadData ;
+
+module.exports = { downloadData, uploadData };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,9 @@
 const express = require('express') ;
-const  uploadData  = require('../controller/file_controller.js');
+//const  uploadData  = require('../controller/file_controller.js');
 const upload = require('../utils/upload.js');
+//const downloadData = require('../controller/file_controller.js')
+
+const { downloadData, uploadData }  = require('../controller/file_controller.js')
 
 const router = express.Router();
 
@@ -8,8 +11,10 @@ const router = express.Router();
 // traffic comes here, then 'uploadData' function called 
 // file_controller have uploadData 
 // upload is a middleware, defined in utils -> upload.js
-router.post('/upload' , upload.single('file') ,  uploadData)
- 
+router.post('/upload' , upload.single('file') ,  uploadData) ;
+
+// To get image after upload
+router.get('/file/:fileId' , downloadData) ;
 
 
 module.exports = router;
